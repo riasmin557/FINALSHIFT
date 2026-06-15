3411a203af3c9a217b7a03a12db2dfdb722c05c8
riasmin557 <riasmin557@gmail.com>
2026-06-14 21:47:43 -0300

    .

--- Industrial/Assets/NewPunch/ShirtlessZombieFree/Scripts/ZumbiSeguir.cs
+++ Industrial/Assets/NewPunch/ShirtlessZombieFree/Scripts/ZumbiSeguir.cs
@@ -5,15 +5,35 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     public Transform player;
     public NavMeshAgent agent;
+    private Animator anim;
+
+    public float distanciaAtaque = 2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(player.position);
+        float distacia=
+            Vector3.Distance(transform.position, player.position);
+
+        if (distacia > distanciaAtaque)
+        {
+            agent.SetDestination(player.position);
+            anim.SetBool("Andar",true);
+        }
+        else
+        {
+            agent.SetDestination(transform.position);
+            anim.SetBool("Andar", false);
+            anim.SetTrigger("Attack");
+        }
+
+       
     }
 }
